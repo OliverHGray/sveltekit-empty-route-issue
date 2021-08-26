d9170849a666f488ac23141c0ca99b6dae643844
Oliver Gray <oliver.h.gray@gmail.com>
2021-08-26 20:00:42 +0100

    Reproduce issue

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+  import { aStore } from './aStore';
+
+  $: if ($aStore) {
+    console.log($aStore);
+  }
+
+  onMount(() => goto('/page2'));
+</script>
---
+++ src/routes/page2.svelte
@@ -0,0 +1,5 @@
+<script>
+  import {aStore} from './aStore';
+</script>
+
+<button on:click={() => $aStore = 'oops'}>Click me</button>
